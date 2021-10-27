58940889921a58fc7bbab9549fedf6079f4aa649
pelincetin <pc2807@columbia.edu>
2021-10-27 23:14:40 +0000

    test

--- rottenpotatoes/spec/models/movie_spec.rb
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -1,3 +1,9 @@
 require 'rails_helper'
+
 describe Movie do
+    describe '.all_ratings' do
+      it 'returns all ratings' do
+        expect(Movie.all_ratings).to eql(["G", "PG", "PG-13", "R"])
+      end
+    end
 end
